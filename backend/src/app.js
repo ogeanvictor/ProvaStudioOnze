892ae4c20a0bf41e48441a6a18c962d628fea4d4
Gean Victor <geanvictorduque@gmail.com>
2023-10-05 18:23:00 -0300

    fix: Fix frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({limit: '200mb', extended: true}))
 app.use(bodyParser.text({ limit: '200mb' }));
 
 const corsOptions = {
-    origin: 'https://provastudioonze.netlify.app',
+    origin: "https://ogeanvictor.github.io/ProvaStudioOnzeFrontend/",
     credentials: true,
 };
 app.use(cors(corsOptions));
