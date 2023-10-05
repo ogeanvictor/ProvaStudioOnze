38ed59dba3038a35045e2830c4af092ce2d3cfa6
Gean Victor <geanvictorduque@gmail.com>
2023-10-05 18:38:50 -0300

    fix: change frontend url vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({limit: '200mb', extended: true}))
 app.use(bodyParser.text({ limit: '200mb' }));
 
 const corsOptions = {
-    origin: "'https://provastudioonze.netlify.app'",
+    origin: "https://prova-studio-onze-mocx.vercel.app/",
     credentials: true,
 };
 app.use(cors(corsOptions));
